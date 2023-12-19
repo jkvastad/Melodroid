3eb1e99e541a4b8f302d7f374be89cc66a64064c
jkvastad <jkvastad@gmail.com>
2023-12-19 18:27:28 +0100

    Installed drywetmidi for writing midi files

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿
+// See https://aka.ms/new-console-template for more information
+using Melanchall.DryWetMidi.Core;
+
 Console.WriteLine("Hello, World!");
+
+//var midifile = MidiFile.Read()
