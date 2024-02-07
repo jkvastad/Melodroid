6a6fc9cf34840543431de691271bf3e9112735b7
jkvastad <jkvastad@gmail.com>
2024-02-07 10:05:51 +0100

    Added explanation of time and length with midifile creattion example for two notes

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Melanchall.DryWetMidi.Common;
 using Melanchall.DryWetMidi.Core;
 using Melanchall.DryWetMidi.Interaction;
 using Melanchall.DryWetMidi.MusicTheory;
@@ -6,15 +7,38 @@ using Melanchall.DryWetMidi.MusicTheory;
 //MIDI standard: http://www.music.mcgill.ca/~ich/classes/mumt306/StandardMIDIfileformat.html
 //Note time in MIDI is defined in header chunk as number of divisions of quarter beat, e.g. setting "division" to 12 means a quarter beat has 12 divisions.
 //Tempo then decides length in seconds of divisions.
-//TODO: write any MIDI file using DryWetMidi and run it in reaper
+/**
+Time and length is poorly explained in docs:
+Time is when something happens, length is for how long:
+    notes.Add(new Melanchall.DryWetMidi.Interaction.Note(NoteName.B, 4)
+    {
+        Velocity = (SevenBitNumber)64,
+        Time = 96,
+        Length = 192,
+    });
+This means "create scientific pitch note B4 at 96 ticks into the midi file, make it 192 ticks long".
+**/
+
+//TODO: write two notes with same ticks but differing tempo - thus differing durations. What is meant by time vs length in drywetmidi?
 //https://github.com/melanchall/drywetmidi#getting-started
 //https://melanchall.github.io/drywetmidi/
 
-//TODO: continue reading time https://melanchall.github.io/drywetmidi/articles/high-level-managing/Time-and-length.html
-
 string folderPath = @"E:\Documents\Reaper Projects\Melodroid\MIDI_write_testing\";
 
-//WriteMIDIWithTimedObjectManager(Path.Combine(folderPath, "midi test start time 2.mid"));
+//TempoMap tempoMap = TempoMap.Create(Tempo.FromBeatsPerMinute(60));
+TempoMap tempoMap = TempoMap.Create(Tempo.FromBeatsPerMinute(60));
+
+//PrintLengthOf(new MetricTimeSpan(hours: 0, minutes: 0, seconds: 10), 0, tempoMap);
+WriteMIDIWithTimedObjectManager(Path.Combine(folderPath, "midi test two notes velocity.mid"));
+
+void PrintLengthOf(ITimeSpan length, long time, TempoMap tempo)
+{
+    Console.WriteLine($"tempo.TimeDivision: {tempo.TimeDivision}");
+    Console.WriteLine($"{nameof(length)}: {length}");
+    Console.WriteLine($"{nameof(time)}: {time}");
+    Console.WriteLine($"tempo.GetTempoAtTime(length).BeatsPerMinute: {tempo.GetTempoAtTime(length).BeatsPerMinute}");
+    Console.WriteLine($"LengthConverter.ConvertFrom(length, time, tempo): {LengthConverter.ConvertFrom(length, time, tempo)}");
+}
 
 void WriteMIDIWithMidiEvents(string fullWritePath)
 {
@@ -27,20 +51,33 @@ void WriteMIDIWithMidiEvents(string fullWritePath)
 void WriteMIDIWithTimedObjectManager(string fullWritePath)
 {
     MidiFile midiFile = new MidiFile();
-    TempoMap tempoMap = midiFile.GetTempoMap();
+    var tempoMap = TempoMap.Create(Tempo.FromBeatsPerMinute(200));
 
     TrackChunk trackChunk = new TrackChunk();
     using (TimedObjectsManager<Melanchall.DryWetMidi.Interaction.Note> notesManager = trackChunk.ManageNotes())
     {
         TimedObjectsCollection<Melanchall.DryWetMidi.Interaction.Note> notes = notesManager.Objects;
-        notes.Add(new Melanchall.DryWetMidi.Interaction.Note(
-            NoteName.A,
-            4,
-            LengthConverter.ConvertFrom(
-                new MetricTimeSpan(hours: 0, minutes: 0, seconds: 10),
-                0,
-                tempoMap)));
+        //notes.Add(new Melanchall.DryWetMidi.Interaction.Note(
+        //    NoteName.A,
+        //    4,
+        //    LengthConverter.ConvertFrom(
+        //        new MetricTimeSpan(hours: 0, minutes: 0, seconds: 10),
+        //        0,
+        //        tempoMap)));
+
+        notes.Add(new Melanchall.DryWetMidi.Interaction.Note(NoteName.A, 4)
+        {
+            Velocity = (SevenBitNumber)64,
+            Time = 0,
+            Length = 192,
+        });
+        notes.Add(new Melanchall.DryWetMidi.Interaction.Note(NoteName.B, 4)
+        {
+            Velocity = (SevenBitNumber)64,
+            Time = 96,
+            Length = 192,
+        });
     }
-    midiFile.Chunks.Add(trackChunk);    
+    midiFile.Chunks.Add(trackChunk);
     midiFile.Write(fullWritePath);
 }
