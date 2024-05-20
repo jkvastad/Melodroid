209b624b62783dc9234c695e3c6a55cdc2cfa35b
jkvastad <jkvastad@gmail.com>
2024-05-20 10:05:54 +0200

    Moved rhythm maker to its own folder

--- BeatBox.cs
+++ BeatBox.cs
@@ -67,33 +67,3 @@ public interface IRhythmMeasureMaker
 {
     public List<int?[]> MakeVelocities();
 }
-
-//Example Usage:
-//int timeDivision = 24;
-//int numberOfMeasures = 32;
-//int beatsPerMeasure = 8;
-//SimpleIsochronicRhythmMaker rhythmMaker = new(timeDivision, numberOfMeasures, beatsPerMeasure);
-public class SimpleIsochronicRhythmMaker(int timeDivision, int numberOfMeasures, int beatsPerMeasure, int velocity = 64) : IRhythmMeasureMaker
-{
-    public int TimeDivision = timeDivision;
-    public int NumberOfMeasures = numberOfMeasures;
-    public int BeatsPerMeasure = beatsPerMeasure;
-    public int Velocity = velocity;
-    public List<int?[]> VelocityMeasures = new();
-
-    public List<int?[]> MakeVelocities()
-    {
-        for (int i = 0; i < NumberOfMeasures; i++)
-        {
-            int?[] velocities = new int?[TimeDivision];
-            int beatsPerDivision = TimeDivision / BeatsPerMeasure;
-            for (int j = 0; j < velocities.Length; j++)
-            {
-                if (j % beatsPerDivision == 0)
-                    velocities[j] = Velocity;
-            }
-            VelocityMeasures.Add(velocities);
-        }
-        return VelocityMeasures;
-    }
-}
